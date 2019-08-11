e1e25b1b1569396a91115f916ad718f08cec74ab
Jonathan Hsieh <jonathan@Jonathans-MacBook-Pro.local>
2019-08-10 17:21:04 -0700

    set up DB

---
+++ server/db/index.js
@@ -0,0 +1,10 @@
+const pgp = require('pg-promise')()
+const port = process.env.PORT || 5000;
+
+
+const db = pgp({
+  host: 'localhost',
+  port: port,
+  database: 'leetcode-study-app',
+  user: 'jonathan'
+});
