c5ac06398089ac3484b8b88545d1aef1f05649c2
Jonathan Hsieh <jonathan@Jonathans-MacBook-Pro.local>
2019-08-11 23:37:28 -0700

    install morgan with minor style fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const app = express();
+const axios = require('axios');
+const morgan = require('morgan');
+
 const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(morgan('dev'))
+
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
